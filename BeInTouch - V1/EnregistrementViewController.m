8fd1af23c51468b8a1d3773fd29d643c97919842
Sonia Ibarra <Sonia-Adela.Ibarra-Flores.1@collegeahuntsic.qc.ca>
2016-04-13 13:40:43 -0400

    Modification de StoryBoard et EnregistrementViewController

--- BeInTouch - V1/EnregistrementViewController.m
+++ BeInTouch - V1/EnregistrementViewController.m
@@ -7,12 +7,34 @@
 //
 
 #import "EnregistrementViewController.h"
+#import "UtilisateurDTO.h"
+#import "UtilisateurFacade.h"
+#import "AdresseDTO.h"
+#import "AdresseFacade.h"
+
 
 @interface EnregistrementViewController ()
+
+
 
 @end
 
 @implementation EnregistrementViewController
+
+#pragma mark - Propriétés
+@synthesize textPrenom;
+@synthesize textNom;
+@synthesize textSexe;
+@synthesize textDateNaissance;
+@synthesize textCourriel;
+@synthesize textTelephone;
+@synthesize textRue;
+@synthesize textAppartement;
+@synthesize textCodePostal;
+@synthesize textVille;
+@synthesize textProvince;
+@synthesize textPays;
+@synthesize idUtilisateur;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -23,10 +45,72 @@
     return self;
 }
 
+
+#pragma mark - Méthodes privées
+- (void)chargerInfo {
+    
+    
+    // Charge les données de l'enregistrement de la base de données
+    UtilisateurDTO *utilisateurDTO = [[UtilisateurFacade utilisateurFacade] readUtilisateur:[self idUtilisateur]];
+    
+    
+    AdresseDTO *adresseU = [[AdresseDTO alloc]init];
+    adresseU = [utilisateurDTO adresse];
+    
+    AdresseDTO *adresseDTO = [[AdresseFacade adresseFacade]readAdresse:[adresseU idAdresse]];
+    
+    //Met a jour les valeurs des text fields de l'interface avec les données de l'enregistrement lu
+    if(utilisateurDTO != nil) {
+        [[self textPrenom]setText:[utilisateurDTO prenom]];
+        [[self textNom] setText:[utilisateurDTO nom]];
+        [[self textSexe] setText:[utilisateurDTO sexe]];
+        [[self textDateNaissance] setText:[utilisateurDTO dateNaissance]];
+        [[self textCourriel] setText:[utilisateurDTO courriel]];
+        [[self textTelephone] setText:[utilisateurDTO telephone]];
+        [[self textRue] setText:[adresseDTO rue]];
+        [[self textAppartement] setText:[adresseDTO appartement]];
+        [[self textCodePostal] setText:[adresseDTO codePostal]];
+        [[self textVille] setText:[adresseDTO ville]];
+        [[self textProvince] setText:[adresseDTO province]];
+        [[self textPays] setText:[adresseDTO pays]];
+        
+        
+        
+        
+        
+        
+        
+        NSLog(@"Requête de lecture réussie pour l'enregistrement %@\n\n", [utilisateurDTO idUtilisateur]);
+    }
+    else {
+        NSLog(@"Impossible d'exécuter la rêquete de lecture\n\n");
+    }
+    
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [[self textNom] setDelegate:self];
+    [[self textPrenom] setDelegate:self];
+    [[self textDateNaissance] setDelegate:self];
+    [[self textCourriel] setDelegate:self];
+    [[self textTelephone] setDelegate:self];
+    [[self textRue] setDelegate:self];
+    [[self textAppartement] setDelegate:self];
+    [[self textCodePostal] setDelegate:self];
+    [[self textVille] setDelegate:self];
+    [[self textProvince] setDelegate:self];
+    [[self textPays] setDelegate:self];
+    
+    
+    // Vérifie si le paramètre de modification d'un enregistrement a été assigné
+    if ([self idUtilisateur] != nil) {
+        // Charge l'enregistrement correspondant
+        [self chargerInfo];
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning
