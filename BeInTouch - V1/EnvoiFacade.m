faa2a566d2f825fa4a3d22ca98a69b96469f4f66
Patrick Tamadje <Patrick-Bertin.Talla-Tamadje.1@collegeahuntsic.qc.ca>
2016-04-13 13:33:00 -0700

    modification de Membre et Envoi (DTO et Facade)

---
+++ BeInTouch - V1/EnvoiFacade.m
@@ -0,0 +1,13 @@
+//
+//  EnvoiFacade.m
+//  BeInTouch - V1
+//
+//  Created by tamadje patrick on 13/04/2016.
+//  Copyright (c) 2016 collegeahuntsic. All rights reserved.
+//
+
+#import "EnvoiFacade.h"
+
+@implementation EnvoiFacade
+
+@end
---
+++ BeInTouch - V1/MembreDTO.m
@@ -0,0 +1,13 @@
+//
+//  MembreDTO.m
+//  BeInTouch - V1
+//
+//  Created by tamadje patrick on 13/04/2016.
+//  Copyright (c) 2016 collegeahuntsic. All rights reserved.
+//
+
+#import "MembreDTO.h"
+
+@implementation MembreDTO
+
+@end
