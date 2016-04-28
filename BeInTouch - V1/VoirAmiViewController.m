8e62bec49deac058ff51dc8b72c86c76e638f970
Patrick Tamadje <Patrick-Bertin.Talla-Tamadje.1@collegeahuntsic.qc.ca>
2016-04-28 06:14:07 -0700

    modification de voirAmisViewController.m

--- BeInTouch - V1/VoirAmiViewController.m
+++ BeInTouch - V1/VoirAmiViewController.m
@@ -8,25 +8,17 @@
 
 #import "VoirAmiViewController.h"
 #import "ContactFacade.h"
-#import "ContactDTO.h"
-#import "UtilisateurDTO.h"
+#import "UtilisateurFacade.h"
 
 @interface VoirAmiViewController ()
 
 #pragma mark - Propriétés privées
-@property (strong, nonatomic) NSMutableArray* amis;
-
-
-#pragma mark - Méthodes privées
-- (void)listerAmis;
+@property (strong,nonatomic) NSMutableArray* membres;
+- (void)chargerDonnees;
 
 @end
 
 @implementation VoirAmiViewController
-
-
-#pragma mark - Propriétés
-@synthesize amis;
 @synthesize idUtilisateur;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -38,85 +30,56 @@
     return self;
 }
 
-- (void)listerAmis {
-    
-    
-    // Charger les données
-    if ([self amis] != nil) {
-        [self setAmis:nil];
+
+
+- (void)chargerDonnees {
+    // Charge les données
+    if ([self membres] != nil) {
+        [self setMembres:nil];
     }
-    
-    [self setAmis:[[ContactFacade contactFacade]getAllContacts:[self idUtilisateur]]];
-        
-       /* ContactDTO* contactDTO = [[ContactDTO alloc] init];
-        
-        for(int i=0 ; i < [ amis count]; ++i) {
-            
-            contactDTO = [amis objectAtIndex:i];
-            
-        }*/
-    
+    [self setMembres:[[ContactFacade contactFacade] getAllContacts:[self idUtilisateur]]];
     // Recharge la table view
     [[self tableView] reloadData];
-    // Dans la méthode qui fait la transaction réseau, après la transaction réseau :
-    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
-    
-    
+    NSLog(@"ajoutMembre idUtilisateur = %@",idUtilisateur);
 }
 
-- (void)viewDidLoad
-{
+
+
+- (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
     [[self tableView] setDelegate:self];
     [[self tableView] setDataSource:self];
-    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
-        // Appel de la méthode qui fait une transaction réseau.
-        [self listerAmis];
-    });
+    [self chargerDonnees];
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - Table view data source
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     
-    return 1;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    
-    return [[self amis] count];
-    
+    return [[self membres] count];
 }
 
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
     return 60.0;
 }
 
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    
+    return 1;
+}
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     
     
-     UtilisateurDTO* amisDTO = [[self amis] objectAtIndex:[indexPath row]];
+    UtilisateurDTO* membresDTO = [[self membres] objectAtIndex:[indexPath row]];
     
     UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"idAmis" forIndexPath:indexPath];
     
@@ -125,16 +88,16 @@
     }
     
     
-    [[cell textLabel] setText:[NSString stringWithFormat:@"%@ %@", [amisDTO prenom], [amisDTO nom]]];
+    [[cell textLabel] setText:[NSString stringWithFormat:@"%@ %@", [membresDTO prenom], [membresDTO nom]]];
     
-     NSLog(@"prenom  %@",[amisDTO prenom ]);
+    
     
     return cell;
 }
 
-
 -(IBAction)returnFromVoirAmis:(UIStoryboardSegue*) segue{
     
 }
+
 
 @end
